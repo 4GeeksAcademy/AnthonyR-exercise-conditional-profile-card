cce846bf64873f20a98b0aefcad307adb7955832
Tonyreng <tonyreng01@gmail.com>
2025-03-29 04:18:38 +0000

    Proyecto terminado

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,6 @@
+import { name } from "file-loader";
 import "../style/index.css";
+import { right } from "@popperjs/core";
 
 /**
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
@@ -33,14 +35,39 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name === null ? "Your name" : variables.name} ${
+    variables.lastName === null ? `Your lastname` : variables.lastName
+  }</h1>
+          <h2>${variables.role === null ? `Your role` : variables.role}</h2>
+          <h3>${variables.city === null ? `Your city` : variables.city}, ${
+    variables.country === null ? `Your country` : variables.country
+  }</h3>
+          <ul class="${
+            variables.socialMediaPosition === null ||
+            variables.socialMediaPosition === right
+              ? `position-right`
+              : variables.socialMediaPosition
+          }">
+            <li><a href="${
+              variables.twitter === null
+                ? `https://twitter.com/4geeksacademy`
+                : `https://twitter.com/${variables.twitter}`
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="${
+              variables.github === null
+                ? `https://github.com/4geeksacademy`
+                : `https://github.com/${variables.github}`
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="${
+              variables.github === null
+                ? `https://linkedin.com/school/4geeksacademy`
+                : `https://linkedin.com/school/${variables.github}`
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="${
+              variables.twitter === null
+                ? `https://instagram.com/4geeksacademy`
+                : `https://instagram.com/${variables.twitter}`
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
